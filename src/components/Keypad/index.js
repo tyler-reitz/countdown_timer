0c2b37ac5be576749ed2f70cb73ee244ff66e12b
Tyler Reitz <tdreitz@gmail.com>
2018-08-07 20:12:13 -0700

    adds Select & tests to Keypad

--- src/components/Keypad/index.js
+++ src/components/Keypad/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react"
 
 class Keypad extends Component {
+
+  handleSelect = (evt) => this.setState({ unit: evt.target.value })
+
   render() {
     return (
       <div>
+        <label for="timer-units">Units: </label>
+        <select onChange={this.handleSelect} id="timer-units" name="timer-units">
+          {['hours','minutes','seconds'].map(unit => 
+            <option key={unit} value={unit}>{unit}</option>
+          )}
+        </select>
+
         {Array(10)
           .fill(null)
           .map((_, idx) => <button key={idx}>{idx}</button>)}
--- src/components/Keypad/index.test.js
+++ src/components/Keypad/index.test.js
@@ -16,4 +16,29 @@ describe('Keypad', () => {
     const wrapper = setup()
     expect(wrapper.find('button').length).toBe(10)
   })
+
+  it('renders a label', () => {
+    const wrapper = setup()
+    expect(wrapper.find('label').length).toBe(1)
+    expect(wrapper.find('label').prop('for')).toBe(wrapper.find('select').prop('id'))
+  })
+
+  it('renders a select field with hrs / mins / secs', () => {
+    const wrapper = setup()
+
+    expect(wrapper.find('select').exists()).toBe(true)
+    expect(wrapper.find('option').length).toBe(3)
+    expect(wrapper.find('select').prop('id')).toBe(wrapper.find('label').prop('for'))
+
+    expect(wrapper.find('option').at(0).prop('value')).toBe('hours')
+    expect(wrapper.find('option').at(0).text()).toBe('hours')
+  })
+
+  it('can set the unit for the keypad', () => {
+    const wrapper = setup()
+
+    wrapper.find('select').simulate('change', { target: { value: 'seconds' } })
+
+    expect(wrapper.state().unit).toBe('seconds')
+  })
 })
